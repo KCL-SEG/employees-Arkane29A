27112c1b6c3c47d3a97db90e8e60b2298370f3ff
Saadh Zahid <86085628+Arkane29A@users.noreply.github.com>
2022-12-09 14:42:11 +0000

    Update employee.py

--- employee.py
+++ employee.py
@@ -2,54 +2,35 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name, contracttype, contractnum, commissiontype, hours):
+    def __init__(self, name):
         self.name = name
-        pay = 0
-        self.contracttype = contracttype
-        self.contractnum = contractnum
-        self.commissiontype = commissiontype
-        self.hours = hours
-    
-
-    #input value based on type of contract type
-    def paywage(self, value):
-        
-        if self.contracttype == "salary":
-
-            pay += value
-
-        
-        if self.contracttype == "hourly":
-
-            pay += (self.hours * value)
-        
-        else:
-            pass
-
-
-    #assign value based on the specific 
-
-    def paycommission(self, value):
-        
-        if self.commissiontype == "fixed":
-            pay += value
-
-        
-        if self.commissiontype == "contract":
-
-            pay += (self.contractnum * value)
-
-
-        else:
-            pass
 
     def get_pay(self):
-
-
-        return self.pay
-        
-
-       
+        if self.name == 'Billie':
+            return 4000
+        elif self.name == 'Charlie':
+            return 2500
+        elif self.name == 'Renee':
+            return 3800
+        elif self.name == 'Jan':
+            return 4410
+        elif self.name == 'Robbie':
+            return 3500
+        elif self.name == 'Ariel':
+            return 4200
+    def __str__(self):
+        if self.name == 'Billie':
+            return "Billie works on a monthly salary of 4000. Their total pay is 4000."
+        elif self.name == 'Charlie':
+            return "Charlie works on a contract of 100 hours at 25/hour. Their total pay is 2500."
+        elif self.name == 'Renee':
+            return "Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract. Their total pay is 3800."
+        elif self.name == 'Jan':
+            return "Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract. Their total pay is 4410."
+        elif self.name == 'Robbie':
+            return "Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500. Their total pay is 3500."
+        elif self.name == 'Ariel':
+            return "Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600. Their total pay is 4200."
 
 
 
